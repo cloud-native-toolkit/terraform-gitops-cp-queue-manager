148e88411d7348c310e5715238a317cdb51ffce9
kanand3 <keshav.anand@outlook.com>
2022-02-11 22:53:36 +0530

    Temporarily removing output variables.
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -2,25 +2,25 @@
 output "instance_name" {
   description = "The name of the module"
   value       = local.instance_name
-  depends_on  = [null_resource.setup_gitops]
+#  depends_on  = [null_resource.setup_gitops]
 }
 
-output "branch" {
+/*output "branch" {
   description = "The branch where the module config has been placed"
   value       = local.application_branch
-  depends_on  = [null_resource.setup_gitops]
+#  depends_on  = [null_resource.setup_gitops]
 }
 
 output "namespace" {
   description = "The namespace where the module will be deployed"
   value       = var.namespace
-  depends_on  = [null_resource.setup_gitops]
+#  depends_on  = [null_resource.setup_gitops]
 }
 
 output "server_name" {
   description = "The server where the module will be deployed"
   value       = var.server_name
-  depends_on  = [null_resource.setup_gitops]
+#  depends_on  = [null_resource.setup_gitops]
 }
 
 output "layer" {
@@ -33,7 +33,7 @@ output "type" {
   description = "The type of module where the module is deployed"
   value       = local.type
   depends_on  = [null_resource.setup_gitops]
-}
+} */
 
 output "bin_dir" {
   value = local.bin_dir

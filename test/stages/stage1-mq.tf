e0d334d2d9af846f550e832fcb4277079b88602e
kanand3 <keshav.anand@outlook.com>
2022-02-11 19:08:14 +0530

    Temporarily commenting module variables to remove terraform plan error.
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage1-mq.tf
+++ test/stages/stage1-mq.tf
@@ -4,13 +4,13 @@ module gitops-cp-mq {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  kubeseal_cert = module.gitops.sealed_secrets_cert
+  #kubeseal_cert = module.gitops.sealed_secrets_cert
   catalog = module.cp_catalogs.catalog_ibmoperators
-  #platform_navigator_name = module.cp_platform_navigator.name
+  platform_navigator_name = module.cp_platform_navigator.name
 
   # not sure if the remaining at needed.
-  license = module.cp4i-dependencies.mq.license
-  entitlement_key = module.cp_catalogs.entitlement_key
+  #license = module.cp4i-dependencies.mq.license
+  #entitlement_key = module.cp_catalogs.entitlement_key
   channel = module.cp4i-dependencies.mq.channel
   namespace = module.gitops_namespace.name
 }

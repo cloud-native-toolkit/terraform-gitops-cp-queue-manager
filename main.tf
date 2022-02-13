3ef7060cbb47e6915e42d00aa73471375a04d921
kanand3 <keshav.anand@outlook.com>
2022-02-13 12:58:40 +0530

    Removing fields from queue manager definition file.
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- main.tf
+++ main.tf
@@ -1,116 +1,98 @@
 locals {
   base_name          = "ibm-mq"
-  instance_name = "${local.base_name}-instance"
-  bin_dir       = module.setup_clis.bin_dir
+  instance_name      = "${local.base_name}-instance"
+  bin_dir            = module.setup_clis.bin_dir
   instance_chart_dir = "${path.module}/charts/ibm-mq-instance"
-  instance_yaml_dir     = "${path.cwd}/.tmp/${local.instance_name}/chart/${local.instance_name}"
-  layer = "services"
-  type  = "base"
+  instance_yaml_dir  = "${path.cwd}/.tmp/${local.instance_name}/chart/${local.instance_name}"
+  layer              = "services"
+  type               = "base"
   application_branch = "main"
-  layer_config = var.gitops_config[local.layer]
- # values_file = "values-${var.server_name}.yaml"
-  values_file = "values.yaml"
+  layer_config       = var.gitops_config[local.layer]
+  # values_file = "values-${var.server_name}.yaml"
+  values_file        = "values.yaml"
 
   instance_values_content = {
-    ibm-mq-instance = {
-      name = var.instancename
+    mqinstance = {
+      name      = var.instancename
       namespace = var.namespace
-      spec = {
+      spec      = {
         license = {
           accept  = true
           license = var.license
           use     = var.use
         }
         queueManager = {
-          name      = var.qmgrname
-          resources = {
-            limits = {
-              cpu      = var.cpulimits
-              requests = {
-                cpu = var.cpurequests
-              }
-            }
-            availability = {
-              type = "SingleInstance"
-            }
-            }
-          }
-            storage = {
-              defaultClass = var.storageclass
-              queueManager = {
-                type = "persistent-claim"
-              }
-            }
-            version = var.mqversion
-            web     = {
-              enabled = true
-            }
+          name    = var.qmgrname
+          version = var.mqversion
+          web     = {
+            enabled = true
           }
         }
       }
-}
-
-module setup_clis {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
-}
-
-
-module pull_secret {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret"
-
-  gitops_config = var.gitops_config
-  git_credentials = var.git_credentials
-  server_name = var.server_name
-  kubeseal_cert = var.kubeseal_cert
-  namespace = var.namespace
-  docker_username = "cp"
-  docker_password = var.entitlement_key
-  docker_server   = "cp.icr.io"
-  secret_name     = "ibm-entitlement-key"
-}
-
-resource null_resource create_instance_yaml {
-  #depends_on = [null_resource.setup_subscription_gitops]
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/create-yaml.sh '${local.instance_name}' '${local.instance_chart_dir}' '${local.instance_yaml_dir}' '${local.values_file}'"
-
-    environment = {
-      VALUES_CONTENT = yamlencode(local.instance_values_content)
     }
   }
 }
-
-resource null_resource setup_instance_gitops {
-  depends_on = [null_resource.create_instance_yaml]
-
-  triggers = {
-    bin_dir = local.bin_dir
-    name = local.instance_name
-    namespace = var.namespace
-    yaml_dir = local.instance_yaml_dir
-    server_name = var.server_name
-    layer = local.layer
-    type = local.type
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
+  module setup_clis {
+    source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
   }
 
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type=${self.triggers.type} --valueFiles='values.yaml,${local.values_file}'"
 
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
+  module pull_secret {
+    source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret"
+
+    gitops_config   = var.gitops_config
+    git_credentials = var.git_credentials
+    server_name     = var.server_name
+    kubeseal_cert   = var.kubeseal_cert
+    namespace       = var.namespace
+    docker_username = "cp"
+    docker_password = var.entitlement_key
+    docker_server   = "cp.icr.io"
+    secret_name     = "ibm-entitlement-key"
+  }
+
+  resource null_resource create_instance_yaml {
+    #depends_on = [null_resource.setup_subscription_gitops]
+    provisioner "local-exec" {
+      command = "${path.module}/scripts/create-yaml.sh '${local.instance_name}' '${local.instance_chart_dir}' '${local.instance_yaml_dir}' '${local.values_file}'"
+
+      environment = {
+        VALUES_CONTENT = yamlencode(local.instance_values_content)
+      }
     }
   }
 
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+  resource null_resource setup_instance_gitops {
+    depends_on = [null_resource.create_instance_yaml]
 
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
+    triggers = {
+      bin_dir         = local.bin_dir
+      name            = local.instance_name
+      namespace       = var.namespace
+      yaml_dir        = local.instance_yaml_dir
+      server_name     = var.server_name
+      layer           = local.layer
+      type            = local.type
+      git_credentials = yamlencode(var.git_credentials)
+      gitops_config   = yamlencode(var.gitops_config)
+    }
+
+    provisioner "local-exec" {
+      command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type=${self.triggers.type} --valueFiles='values.yaml,${local.values_file}'"
+
+      environment = {
+        GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+        GITOPS_CONFIG   = self.triggers.gitops_config
+      }
+    }
+
+    provisioner "local-exec" {
+      when    = destroy
+      command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+      environment = {
+        GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+        GITOPS_CONFIG   = self.triggers.gitops_config
+      }
     }
   }
-}

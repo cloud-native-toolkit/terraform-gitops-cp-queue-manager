ff0fc2920d8fe5b6182c0e057e15b017dcd1dbfd
kanand3 <keshav.anand@outlook.com>
2022-02-14 21:46:36 +0530

    Restoring working code.
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage2-mq-instance.tf
+++ test/stages/stage2-mq-instance.tf
@@ -8,8 +8,7 @@ module "mq_instance" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  #namespace = module.gitops_namespace.name
-  namespace = "telco-cloud"
+  namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
   entitlement_key = module.cp_catalogs.entitlement_key
   license = module.cp4i-dependencies.mq.license

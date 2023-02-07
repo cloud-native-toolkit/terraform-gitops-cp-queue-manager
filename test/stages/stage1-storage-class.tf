8731ca4cfff602c0ae4b679a49a2bde3d5f5f4f1
Sean Sundberg <seansund@us.ibm.com>
2023-02-07 09:40:36 -0600

    Fix test case
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-storage-class.tf
+++ test/stages/stage1-storage-class.tf
@@ -1,4 +1,6 @@
 module "storage" {
+  source = "github.com/cloud-native-toolkit/terraform-util-storage-class-manager.git"
+
   rwo_storage_class = "ibmc-vpc-block-10iops-tier"
   rwx_storage_class = ""
   file_storage_class = ""
